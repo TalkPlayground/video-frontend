a5e45fbbe08bc320f4bb5a6f0adcd572f9b70d1b
Shivam Kumar <shivam@shivam.kumar>
2022-07-25 14:46:57 +0530

    fix some issue again

--- src/Api/index.js
+++ src/Api/index.js
@@ -20,6 +20,6 @@ export const getQueryString = (params) => {
 };
 
 export const supabase = createClient(
-  process.env.REACT_PUBLIC_SUPABASE_URL,
-  process.env.REACT_PUBLIC_SUPABASE_KEY
+  `${process.env.REACT_PUBLIC_SUPABASE_URL}`,
+  `${process.env.REACT_PUBLIC_SUPABASE_KEY}`
 );
