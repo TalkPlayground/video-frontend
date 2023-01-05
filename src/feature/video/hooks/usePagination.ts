9d1c0de4d3c9a25a5a16f98871d7d85013acfc6a
Shivamdigitalglyde <jwick121998@gmail.com>
2023-01-05 14:21:32 +0530

    length ui issue

--- src/feature/video/hooks/usePagination.ts
+++ src/feature/video/hooks/usePagination.ts
@@ -3,19 +3,17 @@ import { maxViewportVideoCounts } from '../video-layout-helper';
 import { useMount } from '../../../hooks';
 import { Dimension } from '../video-types';
 import { ZoomClient } from '../../../index-types';
+import { isAndroidOrIOSBrowser } from '../../../utils/platform';
 const MAX_NUMBER_PER_PAGE = 9;
 // eslint-disable-next-line import/prefer-default-export
-export function usePagination(zmClient: ZoomClient, dimension: Dimension,selfViewGalleryLayout?:any) {
+export function usePagination(zmClient: ZoomClient, dimension: Dimension, selfViewGalleryLayout?: any) {
   const [page, setPage] = useState(0);
   const [totalSize, setTotalSize] = useState(0);
   const [pageSize, setPageSize] = useState(MAX_NUMBER_PER_PAGE);
   useEffect(() => {
-    const size = Math.min(
-      MAX_NUMBER_PER_PAGE,
-      maxViewportVideoCounts(dimension.width, dimension.height),
-    );
+    const size = Math.min(MAX_NUMBER_PER_PAGE, maxViewportVideoCounts(dimension.width, dimension.height));
     setPageSize(size);
-  }, [dimension,selfViewGalleryLayout]);
+  }, [dimension, selfViewGalleryLayout]);
   const onParticipantsChange = useCallback(() => {
     setTotalSize(zmClient.getAllUser().length);
   }, [zmClient]);
@@ -30,13 +28,19 @@ export function usePagination(zmClient: ZoomClient, dimension: Dimension,selfVie
     };
   }, [zmClient, onParticipantsChange]);
   useEffect(() => {
-    if(selfViewGalleryLayout){
+    if (selfViewGalleryLayout) {
       setTotalSize((prev) => prev - 1);
-    }else{
+    } else {
       setTotalSize(zmClient.getAllUser().length);
     }
-  }, [selfViewGalleryLayout])
-  
+  }, [selfViewGalleryLayout]);
+
+  useEffect(() => {
+    if (isAndroidOrIOSBrowser() && zmClient.getAllUser().length > 1) {
+      setTotalSize((prev) => prev - 1);
+    }
+  }, [zmClient.getAllUser().length]);
+
   useMount(() => {
     setTotalSize(zmClient.getAllUser().length);
   });
@@ -45,6 +49,6 @@ export function usePagination(zmClient: ZoomClient, dimension: Dimension,selfVie
     totalPage: Math.ceil(totalSize / pageSize),
     pageSize,
     totalSize,
-    setPage,
+    setPage
   };
 }
